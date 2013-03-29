f5eb62d7ef1a600d66f753f8253841a3223d3a84
Sebastián J. Seba <ssebastianj@gmail.com>
2013-03-29 13:50:24 -0300

    Agregadas clases para modelar GridWorld y Estado

---
+++ src/core/estado/estado.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Estado(object):
+    """Clase Estado"""
+    def __init__(self, ancho, alto, estados=None):
+        super(Estado, self).__init__()
+        self._ancho = ancho
+        self._alto = alto
+        self._estados = estados
+
+    def _get_ancho(self):
+        return self._ancho
+
+    def _set_ancho(self, valor):
+        self._ancho = valor
+
+    ancho = property(_get_ancho, _set_ancho, None, "Ancho del Estado")
+
+    def _get_alto(self):
+        return self._alto
+
+    def _set_alto(self, valor):
+        self._alto = valor
+
+    alto = property(_get_alto, _set_alto, None, "Alto del Estado")
+
+    def _get_estados(self):
+        return self._estados
+
+    def _set_estados(self, valor):
+        self._estados = valor
+
+    estados = property(_get_estados, _set_estados, None, "Estados del Estado")
