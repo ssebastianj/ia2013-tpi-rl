355647516b161761efa36541fc299f8d0d2a6654
Sebastián J. Seba <ssebastianj@gmail.com>
2013-04-19 17:56:25 -0300

    Eliminado archivo innecesario.
    Signed-off-by: Sebastián J. Seba <ssebastianj@gmail.com>

--- src/gui/qtgen/mainwindow.py
+++ src/gui/qtgen/mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'D:\Sebastian\Mis documentos\Programacion\Proyectos\IA2013TPIRL\gui\qt\IA2013TPIRLGUI\mainwindow.ui'
 #
-# Created: Fri Apr 19 17:44:28 2013
+# Created: Fri Apr 19 17:55:54 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
