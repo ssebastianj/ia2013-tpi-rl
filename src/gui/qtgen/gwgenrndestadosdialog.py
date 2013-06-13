7f6b98adc13ebf01e64ab74e8545be0f3aced29c
Lucia Vallejos <luvallejos@hotmail.com>
2013-06-13 16:37:48 -0300

    Agregados decimales a la diferencia mínima.
    Signed-off-by: Lucia Vallejos <luvallejos@hotmail.com>

--- src/gui/qtgen/gwgenrndestadosdialog.py
+++ src/gui/qtgen/gwgenrndestadosdialog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\Sebastian\Mis documentos\Programacion\Proyectos\IA2013TPIRL\gui\qt\IA2013TPIRLGUI\gwgenrndestadosdialog.ui'
+# Form implementation generated from reading ui file 'C:\Users\Lucia\workspace\TP-IA\gui\qt\IA2013TPIRLGUI\gwgenrndestadosdialog.ui'
 #
-# Created: Tue Jun 11 13:01:09 2013
+# Created: Thu Jun 13 16:42:38 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
